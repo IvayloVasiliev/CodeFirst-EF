f04d83881c8fde0cc3282dbbe98eab6740c5eb03
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-04 17:24:21 +0200

    Add project files.

---
+++ P01_HospitalDatabase.Data/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace P01_HospitalDatabase.Data
+{
+    public class Config
+    {
+        public const string ConnectionString = "Server=DESKTOP-4Q4QNM5\\SQLEXPRESS;Database=Hospital;Integrated Security=True;";
+    }
+}
