fa5e9ec77573c9a0b7d381600ee276f81ee15a4b
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-05 17:05:47 +0200

    Problem 03 Creating Sales Database

---
+++ SalesDatabase/Data/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    public class Config
+    {
+        public const string ConnectionString = "Server=DESKTOP-4Q4QNM5\\SQLEXPRESS;Database=Sales;Integrated Security=True;";
+    }
+}
---
+++ SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,102 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Models;
+    using System;
+
+    public class SalesContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Store> Stores { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Config.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            this.OnModelCreatingProduct(modelBuilder);
+
+            this.OnModelCreatingCustomer(modelBuilder);
+
+            this.OnModelCreatingStore(modelBuilder);
+
+            this.OnModelCreatingSale(modelBuilder);
+        }
+
+        private void OnModelCreatingSale(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+              .Entity<Sale>()
+              .HasKey(s => s.SaleId);
+
+
+        }
+
+        private void OnModelCreatingStore(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+              .Entity<Store>()
+              .HasKey(s => s.StoreId);
+
+            modelBuilder
+               .Entity<Store>()
+               .Property(c => c.Name)
+               .HasMaxLength(80)
+               .IsUnicode();
+
+            modelBuilder
+               .Entity<Store>()
+               .HasMany(s => s.Sales)
+               .WithOne(x => x.Store);
+
+        }
+
+        private void OnModelCreatingCustomer(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+              .Entity<Customer>()
+              .HasKey(c => c.CustomerId);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsUnicode();
+
+            modelBuilder
+               .Entity<Customer>()
+               .Property(c => c.Email)
+               .HasMaxLength(80);
+
+            modelBuilder
+                .Entity<Customer>()
+                .HasMany(c => c.Sales)
+                .WithOne(s => s.Customer);
+
+        }
+
+        private void OnModelCreatingProduct(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+               .Entity<Product>()
+               .HasKey(p => p.ProductId);
+
+            modelBuilder
+                .Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Product>()
+                .HasMany(p => p.Sales)
+                .WithOne(s => s.Product);
+        }
+    }
+}
